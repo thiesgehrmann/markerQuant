f56c199b23d57c91531f067b171a8b8db656badd
thiesgehrmann <thiesgehrmann@gmail.com>
2017-06-22 16:27:47 +0200

    Added example data

---
+++ testData/gen_expr_data.R
@@ -0,0 +1,14 @@
+library(polyester)
+library(Biostrings)
+
+args = commandArgs(trailingOnly=TRUE)
+
+fasta_file = args[1]
+fasta = readDNAStringSet(fasta_file)
+
+fold_changes = head(matrix(c(rep(c(1,4),length(fasta)),rep(c(4,1),length(fasta))),nrow=length(fasta)*2),length(fasta))
+
+readspertx = round(20 * width(fasta) / 100)
+
+simulate_experiment(fasta_file, reads_per_transcript=readspertx, 
+    num_reps=c(2,2), fold_changes=fold_changes, outdir='simulated_reads')
